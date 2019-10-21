c2d8f1851865227903546dab63b08d8008824103
IYotova <iliyanayotova@bennington.edu>
2019-10-21 08:49:12 +0100

    Added trainer class

--- src/com/spartaglobal/Sparta_Academy/Trainers.java
+++ src/com/spartaglobal/Sparta_Academy/Trainers.java
@@ -1,4 +1,13 @@
 package com.spartaglobal.Sparta_Academy;
 
-public class Trainers {
+public class Trainers extends Employee{
+
+
+    public Trainers(String name, double salary, int age, String gender, String location) {
+        super(name, salary, age, gender, location);
+    }
+
+
 }
+
+
